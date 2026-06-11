c7996240f871978b3a3d253a05d56934c36440bf
YamunaPatoju <155348240+YamunaPatoju@users.noreply.github.com>
2026-06-11 07:55:50 +0530

    Add mergeArrays function for in-place merging
    Implemented a function to merge two sorted arrays without using extra space.

---
+++ arrays/arrays/merge_without_extra_space/merge_without_extra_space.py
@@ -0,0 +1,15 @@
+def mergeArrays(self, a, b):
+        i = len(a) - 1
+        j = 0
+
+        while i >= 0 and j < len(b):
+
+            if a[i] > b[j]:
+                a[i], b[j] = b[j], a[i]
+                i -= 1
+                j += 1
+            else:
+                break
+
+        a.sort()
+        b.sort()
