13e8ac8d83177865b046e4f4910fc5002a67876a
Patoju Yamuna <155348240+YamunaPatoju@users.noreply.github.com>
2026-09-17 23:15:45 +0530

    Create search_in_bst.py

---
+++ bst/search_in_bst/search_in_bst.py
@@ -0,0 +1,12 @@
+class Solution:
+    def search(self, root, key):
+        if root is None:
+            return False
+
+        if root.data == key:
+            return True
+
+        if key < root.data:
+            return self.search(root.left, key)
+
+        return self.search(root.right, key)
