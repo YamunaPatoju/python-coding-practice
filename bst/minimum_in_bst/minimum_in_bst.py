59eafb7f6ce27a46f38d8f3ef636bef067fe5fc9
Patoju Yamuna <155348240+YamunaPatoju@users.noreply.github.com>
2026-09-19 23:59:55 +0530

    Create minimum_in_bst.py

---
+++ bst/minimum_in_bst/minimum_in_bst.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minValue(self, root):
+        if root is None:
+            return -1
+
+        while root.left:
+            root = root.left
+
+        return root.data
+
+  
